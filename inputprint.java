3d688536f7243fbfaf25e98c28e891816f4d5d3e
phollyerviggiani <patrick.hollyerviggiani@dcmail.ca>
2024-01-23 18:25:59 -0500

    New Branch, first commit

--- inputprint.java
+++ inputprint.java
@@ -5,7 +5,11 @@ class InputPrint{
         Scanner scanner = new Scanner(System.in); // Creates the scanner object
         System.out.println("Enter your name: "); // Print Line
         String name = scanner.nextLine(); //take the user input
-        System.out.println("Hello my name is " + name + ", Welcome to Object Oriented Programming (OOP)"); // displaying output
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter your student ID: ");
+        String studentid = scanner2.nextLine();
+        System.out.println("Hello my name is " + name + studentid + ", Welcome to Object Oriented Programming (OOP)"); // displaying output
         scanner.close(); // closing scanner
+        scanner2.close();
     }
 }
